7dedec089afac8eafa3f2765d925acc75bb042b3
Ruzindana <67051049+Ruzindana@users.noreply.github.com>
2020-07-17 09:18:28 +0300

    initial commit agenda

---
+++ main.js
@@ -0,0 +1,32 @@
+var main = document.querySelector('.main');
+main.style.color = 'blue' ,main.style.textAlign = 'center'
+
+var topic1 = document.querySelector('.topic1');
+topic1.style.color = 'cyan'
+
+var topic2 = document.querySelector('.topic2');
+topic2.style.color = 'cyan'
+
+var topic3 = document.querySelector('.topic3');
+topic3.style.color = 'cyan'
+
+var topic4 = document.querySelector('.topic4');
+topic4.style.color = 'cyan'
+
+var bottom = document.querySelector('.bottom');
+bottom.style.color = 'blue'
+
+var l1 = document.querySelector('.l1');
+l1.style.color = '#43ed12'
+
+
+var l2 = document.querySelector('.l2');
+l2.style.color = '#43ed12'
+
+
+var l3 = document.querySelector('.l3');
+l3.style.color = '#43ed12'
+
+
+var l4 = document.querySelector('.l4');
+l4.style.color = '#43ed12'
